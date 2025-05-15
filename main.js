ad3c477e2d8812aa649c081426954a5191924814
AlexPamdo <alexmegasuper5@gmail.com>
2025-05-15 18:30:25 -0400

    new

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ $(function () {
         const filename = src.split('/').pop().split('.')[0];
         const $card = $(
           `<div class="bg-zinc-800 rounded-lg overflow-hidden shadow-lg hover:scale-105 transition-transform duration-300">
-             <img src="${src}" alt="${filename}" class="w-full h-64 object-cover">
+             <img src="${src}" alt="${filename}" class="w-full h-full object-cover">
              <div class="p-4">
                <h3 class="text-lg font-bold mb-1">${filename}</h3>
              </div>
